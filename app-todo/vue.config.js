76f78a393f5d18351545a83b76e46cfd41dfa047
Gabriel Teixeira Dias <g3briel58@gmail.com>
2022-09-21 15:55:41 +0000

    suppression fichier inutile node

--- app-todo/vue.config.js
+++ app-todo/vue.config.js
@@ -2,3 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    allowedHosts: "all",
+    client: {
+      webSocketURL: {
+        port: process.env.GITPOD_WORKSPACE_ID ? 443 : undefined,
+      },
+    },
+  },
+};
